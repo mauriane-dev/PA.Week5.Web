5ac6ce5fa452479fa8b693fa2667b1963ac7ab16
Alessandra Degan Di Dieco <alessandra.degan@icubed.it>
2021-12-16 09:24:26 +0100

    es 1 parte 3 - altri punti

---
+++ EsercitazioneHotel/hotel.js
@@ -0,0 +1,20 @@
+﻿
+function welcome() {
+    let username = localStorage.getItem("username");
+
+    if (username != null)
+        alert("Benvenuto " + username);
+    else {
+        alert("Benvenuto!")
+    }
+}
+
+function openForm() {
+    let form = document.getElementById("myForm")
+    form.style.display = "block";
+}
+
+function closeForm() {
+    let form = document.getElementById("myForm")
+    form.style.display = "none";
+}
